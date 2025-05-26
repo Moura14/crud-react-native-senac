cf46bf52f5bacc60dc3df1a849759f089bdc49c8
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-26 15:46:27 -0300

    feat: navegação stack #02

--- app/App.tsx
+++ app/App.tsx
@@ -1,18 +1,47 @@
+import { Ionicons } from '@expo/vector-icons'
 import { createNativeStackNavigator } from '@react-navigation/native-stack'
 import React from 'react'
+import { TouchableOpacity } from 'react-native'
 import UserForm from './views/UserForm'
 import UserList from './views/UserList'
 
-
-
 const Stack = createNativeStackNavigator()
-
 
 export default (_props: any) => {
     return (
-            <Stack.Navigator initialRouteName='UserList'>
-                <Stack.Screen name='UserList' component={UserList}></Stack.Screen>
-                 <Stack.Screen name='UserForm' component={UserForm}></Stack.Screen>
-            </Stack.Navigator>
+        <Stack.Navigator initialRouteName='UserList' screenOptions={screenOptions}>
+            <Stack.Screen
+                name='UserList'
+                component={UserList}
+                options={({ navigation }) => ({
+                    title: 'Lista de Usuários',
+                    headerRight: () => (
+                        <TouchableOpacity
+                            style={{ marginRight: 10 }}
+                            onPress={() => navigation.navigate('UserForm')}
+                        >
+                            <Ionicons name='add' size={25} color='white' />
+                        </TouchableOpacity>
+                    ),
+                })}
+            />
+            <Stack.Screen
+                name='UserForm'
+                component={UserForm}
+                options={{
+                    title: 'Formulário de Usuários',
+                }}
+            />
+        </Stack.Navigator>
     )
+}
+
+const screenOptions = {
+    headerStyle: {
+        backgroundColor: '#f4511e',
+    },
+    headerTintColor: '#fff',
+    headerTitleStyle: {
+        fontWeight: 'bold',
+    },
 }
