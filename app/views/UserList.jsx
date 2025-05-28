3804f6f547740bd9983eaa55f6d51d71dfaaca97
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-28 09:14:35 -0300

    feat: lista de usuários #02

--- app/views/UserList.jsx
+++ app/views/UserList.jsx
@@ -1,21 +1,45 @@
 import React from 'react'
-import { FlatList, View } from 'react-native'
-import { Avatar, ListItem } from 'react-native-elements'
+import { Alert, FlatList, View } from 'react-native'
+import { Avatar, Button, Icon, ListItem } from 'react-native-elements'
 import users from '../data/user'
 
 
 export default props => {
 
-    console.warn(users)
+    function confirmUserDeletion(user){
+        Alert.alert('Excluir usuário', 'Deseja excluir o usuário?', [
+            {
+                text: 'Sim',
+                onPress(){
+                    console.warn('delete' + user.id)
+                }
+            },
+            {
+                text: 'Não'
+            }
+        ])
+    }
 
     function getUserItem({item: user}){
         return (
-            <ListItem bottomDivider onPress={() => props.navigation.navigate('UserForm')}>
+            <ListItem bottomDivider  onPress={() => props.navigation.navigate('UserForm')}>
                 <Avatar source={{uri: user.avatar}}></Avatar>
                 <ListItem.Content>
                     <ListItem.Title>{user.name}</ListItem.Title>
                     <ListItem.Title>{user.email}</ListItem.Title>
                 </ListItem.Content>
+            <Button
+            onPress={() => {
+            props.navigation.navigate('UserForm', user);
+          }}
+            type="clear"
+            icon={<Icon name="edit" size={25} color="orange" />}
+        />
+         <Button 
+            onPress={() => {confirmUserDeletion(user)}}
+            type="clear"
+            icon={<Icon name="delete" size={25} color="red"/>}
+        />
             </ListItem>
         )
     }
