178a88875308c9a130fd3903f70a4f8c18f20a94
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-28 14:36:35 -0300

    feat: formulário de usuário

--- app/views/UserForm.jsx
+++ app/views/UserForm.jsx
@@ -1,11 +1,41 @@
 import { useState } from 'react'
-import { Text } from 'react-native'
+import { StyleSheet, Text, TextInput, View } from 'react-native'
+import { Button } from 'react-native-elements'
 
 
 export default ({route, navigation}) => {
     
   const [user, setUser] = useState(route.params ? route.params : {})
     return (
-        <Text>{user.id}</Text>
+       <View style={style.form}>
+            <Text>Name</Text>
+            <TextInput style={style.input} onChangeText={name => setUser({...user, name})} placeholder='Informe o Nome' value={user.name}></TextInput>
+            <View>
+                <Text>Email</Text>
+                <TextInput style={style.input} onChangeText={email => setUser({...user, email})} placeholder='Informe seu Email' value={user.email}></TextInput>
+            </View>
+            <View>
+                <Text>URL do Avatar</Text>
+                <TextInput style={style.input} onChangeText={url => setUser({...user, email})} placeholder='Informe a URl do Avatar' value={user.avatar}>
+                </TextInput>
+            </View>
+            <Button title="Salvar" onPress={() =>  {
+                navigation.goBack()
+            }}>
+
+            </Button>
+       </View>
     )
 }
+
+const style = StyleSheet.create({
+    form: {
+        padding: 15
+    },
+    input: {
+        height: 40,
+        borderColor:'gray' ,
+        borderWidth: 1,
+        marginBottom: 10
+    }
+})
