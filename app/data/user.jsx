a0af272ccf59b2f6c749b8411c4ab9e57e77c028
João Gabriel <87152738+Moura14@users.noreply.github.com>
2025-05-28 09:01:53 -0300

    feat: lista de usuários #01

---
+++ app/data/user.jsx
@@ -0,0 +1,26 @@
+export default [
+    {
+        id: 1,
+        name: 'João Silva',
+        email: 'joaosilva@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2017/02/25/22/04/user-icon-2098873_1280.png'
+    },
+    {
+        id: 2,
+        name: 'Thierry Henry',
+        email: 'thierry@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2017/02/25/22/04/user-icon-2098873_1280.png'
+    },
+    {
+        id: 3,
+        name: 'Sandrey',
+        email: 'sandrey@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2017/02/25/22/04/user-icon-2098873_1280.png'
+    },
+    {
+        id: 4,
+        name: 'Neymar Jr',
+        email: 'neymar@gmail.com',
+        avatar: 'https://cdn.pixabay.com/photo/2017/02/25/22/04/user-icon-2098873_1280.png'
+    }
+]
--- app/views/UserList.jsx
+++ app/views/UserList.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-import { Text } from 'react-native'
+import { FlatList, View } from 'react-native'
+import { Avatar, ListItem } from 'react-native-elements'
+import users from '../data/user'
 
 
 export default props => {
+
+    console.warn(users)
+
+    function getUserItem({item: user}){
+        return (
+            <ListItem bottomDivider onPress={() => props.navigation.navigate('UserForm')}>
+                <Avatar source={{uri: user.avatar}}></Avatar>
+                <ListItem.Content>
+                    <ListItem.Title>{user.name}</ListItem.Title>
+                    <ListItem.Title>{user.email}</ListItem.Title>
+                </ListItem.Content>
+            </ListItem>
+        )
+    }
+
+
     return (
-        <Text>UserList</Text>
+        <View>
+            <FlatList data={users} keyExtractor={user => user.id.toString()} renderItem={getUserItem}>
+
+            </FlatList>
+        </View>
     )
 }
